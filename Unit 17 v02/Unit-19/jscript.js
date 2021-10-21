00bef16a8bb8d6e8cbc32f601315ca52da8abdc5
amanning98 <aamanning@alumni.fullsail.edu>
2021-10-21 17:37:22 -0400

    update

---
+++ Unit 17 v02/Unit-19/jscript.js
@@ -0,0 +1,18 @@
+
+  $(".aniamtion1").hover(function(){
+   console.log('test');
+    $(".image1").fadeToggle();
+    $(".imgSea").fadeToggle();
+    });
+ $(".aniamtion2").hover(function(){
+   console.log('test');
+    $(".image2").fadeToggle();
+    $(".imgOregon").fadeToggle();
+    });
+ $(".aniamtion3").hover(function(){
+   console.log('test');
+    $(".image3").fadeToggle();
+    $(".imgTravel").fadeToggle();
+    });
+
+
